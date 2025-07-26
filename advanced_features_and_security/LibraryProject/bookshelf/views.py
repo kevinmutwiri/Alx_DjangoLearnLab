3043d1dffaebbc4b42f92f1a82bfc216863d8dc0
kevinmutwiri <keivnmutwiri@gmail.com>
2025-07-26 09:54:50 +0300

    Add form to view

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -3,6 +3,7 @@ from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from .models import Book
+from .forms import ExampleForm # Added this import
 
 class BookListView(LoginRequiredMixin, PermissionRequiredMixin, ListView):
     model = Book
